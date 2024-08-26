bc816d5fc6b849509185294f5a4740c8c76246f9
Antonio Segalini <132697545+AntSegalini@users.noreply.github.com>
2024-08-26 13:24:15 +0200

    Add files via upload

--- file_details.py
+++ file_details.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 
-root_path='/home/antonio/Documents/Messori'
+root_path='/home/antonio/Documents/Messori/final_code'
 
 ####################################################################
 
@@ -21,17 +21,23 @@ def filePath_trend_climatology():
 
 def filePath_mask():
     # file and path where the land-sea mask is stored
-    datapath=os.path.join(root_path,'ERA5','surface')
+    datapath=os.path.join(root_path,'ERA5_mask','surface')
     return os.path.join(datapath,'lsm_0.5x0.5.pkl')
 
 ####################################################################
 
 def filePath_mask_ERA5():
     # file and path where the land-sea mask is stored
-    datapath=os.path.join(root_path,'ERA5','surface','lsm_1279l4_0.1x0.1.grb_v4_unpack.nc') # original mask file
+    datapath=os.path.join(root_path,'ERA5_mask','surface','lsm_1279l4_0.1x0.1.grb_v4_unpack.nc') # original mask file
     original_resolution=0.1                                                                 # resolution of the original mask
     desired_resolution=0.5                                                                  # resolution of the desired mask
-    root_path_loc=os.path.join(root_path,'ERA5','surface')                                  # path where the mask is stored
+    root_path_loc=os.path.join(root_path,'ERA5_mask','surface')                                  # path where the mask is stored
     return datapath , original_resolution , desired_resolution , root_path_loc
 
 ####################################################################
+
+def start_calendar_day():
+    # number of seconds at 1/1/1980 (central day of the analysis) from 1/1/1970 (origin of the ERA5 dataset)
+    # obtained from (dt.datetime(1980,1,1)-dt.datetime(1970,1,1)).total_seconds()
+    return 315532800    
+    
